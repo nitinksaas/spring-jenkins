906e71112a319046a44e804501ac6e6897ff8eb8
nitinksaas <nitink.saas@gmail>
2026-04-13 10:53:09 +0530

    jenkins testing console new

--- src/main/java/com/example/demo/ApacheKafkaProjectApplication.java
+++ src/main/java/com/example/demo/ApacheKafkaProjectApplication.java
@@ -3,11 +3,20 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ApacheKafkaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApacheKafkaProjectApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+
+		System.out.println("testing on startup 123...");
+
 	}
 
 }
